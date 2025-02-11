3d0f206635815244103905817d26104c4e1ecd1a
judithgemma <judithgemma17@gmail.com>
2025-02-11 20:08:02 +0200

    multiply 2 to any entered number

---
+++ multiply_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, result;
+    
+    // Prompt user for input
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    // Multiply the number by 2
+    result = num * 2;
+    
+    // Display the result
+    printf("The result is: %d\n", result);
+    
+    return 0;
+}
